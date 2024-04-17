99402a0b133221a352e2a128fcba1e1a474b0d64
Aflaungos <31782624+Aflaungos@users.noreply.github.com>
2024-04-17 17:25:40 -0300

    AndroidProducts.mk: Adapt for Lineage-21 (QRP2)

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -18,6 +18,6 @@ PRODUCT_MAKEFILES := \
     $(LOCAL_DIR)/lineage_m23xq.mk
 
 COMMON_LUNCH_CHOICES := \
-    lineage_m23xq-userdebug \
-    lineage_m23xq-user \
-    lineage_m23xq-eng
+    lineage_m23xq-ap1a-userdebug \
+    lineage_m23xq-ap1a-user \
+    lineage_m23xq-ap1a-eng
