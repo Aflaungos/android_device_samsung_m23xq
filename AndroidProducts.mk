c43c5735203dd4f3bf8b26bbd1546583490b11d9
Aflaungos <31782624+Aflaungos@users.noreply.github.com>
2024-04-26 14:33:33 -0300

    AndroidProducts.mk: Update again for A14 QPR2

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -18,6 +18,6 @@ PRODUCT_MAKEFILES := \
     $(LOCAL_DIR)/lineage_m23xq.mk
 
 COMMON_LUNCH_CHOICES := \
-    lineage_m23xq-ap1a-userdebug \
-    lineage_m23xq-ap1a-user \
-    lineage_m23xq-ap1a-eng
+    lineage_m23xq-sp2a-userdebug \
+    lineage_m23xq-sp2a-user \
+    lineage_m23xq-sp2a-eng
